46ca53c6a3d698ff069c3dc3a2f4ad5bef3eaba5
cmchappuis <caetano.chappuis@stud.fhgr.ch>
2023-11-01 13:22:37 +0100

    creating header working

---
+++ js/header-element.js
@@ -0,0 +1,16 @@
+class HeaderElement extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <header class="header">
+            <nav class="nav">
+                <ul class="nav-list">
+                    <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="/species-list" class="nav-link">Species List</a></li>
+                    <li class="nav-item"><a href="/about" class="nav-link">About</a></li>
+                </ul>
+            </nav>
+        </header>
+      `;
+    }
+}
+customElements.define('header-element', HeaderElement);
