9128c94025d3bfb81990141896be0a0cc43aee47
cmchappuis <caetano.chappuis@stud.fhgr.ch>
2023-11-14 10:49:16 +0100

    clean up

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ let model;
 
 // function to load the model
 async function loadModel() {
-    // Load the model using the Teachable Machine library
     model = await tmImage.load(modelURL + 'model.json', modelURL + 'metadata.json');
     console.log('Model loaded');  
 }
@@ -21,17 +20,14 @@ const resultSection = document.getElementById("result");
 const speciesElement = document.getElementById("species");
 const uploadButton = document.querySelector('button[type="submit"]');
 
-// Button apear and disapear 
+// Button appear and disappear 
 imageInput.addEventListener('change', function() {
     if (this.files.length > 0) {
-        // If a file is chosen, display the upload button by removing the 'hidden' class
         uploadButton.classList.remove('hidden');
     } else {
-        // If no file is chosen (e.g., if the user cancels the file picker), hide the button
         uploadButton.classList.add('hidden');
     }
 });
-
 
 // Object to map species names to their respective IDs
 const speciesLookup = {
@@ -48,52 +44,35 @@ function getSpeciesID(speciesName) {
 uploadForm.addEventListener("submit", async function (e) {
     e.preventDefault();  
 
-    // Get the image element
-    const image = document.getElementById('preview-image');  
+    const image = document.getElementById('center-image'); // Target the center image
     if (!model) {
         console.error('Model not loaded'); 
         return;
     }
 
-    // Classify the image using the loaded model
     const prediction = await model.predict(image);
     console.log('Prediction:', prediction);  
-    
-    // Sort the prediction array by probability in descending order
     prediction.sort((a, b) => b.probability - a.probability);
 
-    // Get the species name from the highest probability prediction
     const speciesName = prediction[0].className.replace(/_/g, ' '); 
     console.log('Species Name:', speciesName);  
 
-    // Get the species ID using the species name
     const speciesID = getSpeciesID(speciesName);
     console.log('Species ID:', speciesID); 
 
-    // Construct the URL for the species information page
     const infoPageURL = `https://im.chappuiscaetano.ch/php/species.php?id=${speciesID}`;
-    
-    // Redirect the user to the species information page
     window.location.href = infoPageURL;
 });
 
 // Function to display a file preview when a file is selected
 function previewFile() {
-    console.log('previewFile called');  // Debug log
-    var previewImage = document.getElementById("preview-image");
+    var centerImage = document.getElementById("center-image"); // Target the center image
     var fileInput = document.getElementById("image-upload");
-    var introText = document.getElementById("intro-text");
-    var previewHeading = document.getElementById("preview-heading");
-    var filePreview = document.getElementById("file-preview");  // Get a reference to the file-preview div
 
-    if (fileInput.files && fileInput.files[0]) {
+    if (fileInput.files && fileFile.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
-            previewImage.src = e.target.result;
-            localStorage.setItem('uploadedImage', e.target.result);  // Store image data to localStorage
-            introText.style.display = "none"; 
-            previewHeading.style.display = "block";  
-            filePreview.style.display = "block";  // Set the display property of file-preview to block
+            centerImage.src = e.target.result; // Update the src of the center image
         };
         reader.readAsDataURL(fileInput.files[0]);
     }
