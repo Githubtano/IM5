93cbcdc531a7d3885a3b2b9e30c316eb4f41c44e
cmchappuis <caetano.chappuis@stud.fhgr.ch>
2023-11-16 17:02:38 +0100

    fixed preview
    fixed the preview function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 // Define the URL of the model
 const modelURL = 'https://teachablemachine.withgoogle.com/models/ORHSpBtYq/';
 
-// variable to hold the loaded model
+// Declare a variable to hold the loaded model
 let model;
 
 // function to load the model
 async function loadModel() {
-
-    // Load the model using the Teachable Machine library
     model = await tmImage.load(modelURL + 'model.json', modelURL + 'metadata.json');
     console.log('Model loaded');  
 }
@@ -33,6 +31,7 @@ imageInput.addEventListener('change', function() {
     }
 });
 
+
 // Object to map species names to their respective IDs
 const speciesLookup = {
     "Amphiprion ocellaris": 1,
@@ -48,66 +47,50 @@ function getSpeciesID(speciesName) {
 uploadForm.addEventListener("submit", async function (e) {
     e.preventDefault();  
 
-    // Get the image element
-    const image = document.getElementById('preview-image');  
+    const image = document.getElementById('center-image'); // Target the center image
     if (!model) {
         console.error('Model not loaded'); 
         return;
     }
+    // Classify the image using the loaded model
+    const prediction = await model.predict(image);
+    console.log('Prediction:', prediction);  
+    
+    // Sort the prediction array by probability in descending order
+    prediction.sort((a, b) => b.probability - a.probability);
 
+    // Get the species name from the highest probability prediction
+    const speciesName = prediction[0].className.replace(/_/g, ' '); 
+    console.log('Species Name:', speciesName);  
+
+    // Get the species ID using the species name
+    const speciesID = getSpeciesID(speciesName);
+    console.log('Species ID:', speciesID); 
+
+    // Construct the URL for the species information page
+    const infoPageURL = `https://im.chappuiscaetano.ch/php/species.php?id=${speciesID}`;
+    
+    // Redirect the user to the species information page
+    window.location.href = infoPageURL;
 });
 
 // Function to display a file preview when a file is selected
 function previewFile() {
-    console.log('previewFile called');  
-
-    var centerImage = document.getElementById("center-image");  
+    console.log('previewFile called');  // Debug log
+    var centerImage = document.getElementById("center-image"); // Get the center image element
     var fileInput = document.getElementById("image-upload");
     var introText = document.getElementById("intro-text");
     var previewHeading = document.getElementById("preview-heading");
-    var filePreview = document.getElementById("file-preview");  
+    var filePreview = document.getElementById("file-preview");  // Get a reference to the file-preview div
 
     if (fileInput.files && fileInput.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
-            // Set the image source to the result from FileReader
-            centerImage.src = e.target.result;
-
-            // Event listener for when the image is fully loaded
-            centerImage.onload = async function() {
-                // Ensure the model is loaded
-                if (!model) {
-                    console.error('Model not loaded'); 
-                    return;
-                }
-
-                // Now that the image is loaded, perform the prediction
-                const prediction = await model.predict(centerImage);
-                console.log('Prediction:', prediction);  
-
-                // Sort the prediction array by probability in descending order
-                prediction.sort((a, b) => b.probability - a.probability);
-
-                // Get the species name from the highest probability prediction
-                const speciesName = prediction[0].className.replace(/_/g, ' '); 
-                console.log('Species Name:', speciesName);  
-
-                // Get the species ID using the species name
-                const speciesID = getSpeciesID(speciesName);
-                console.log('Species ID:', speciesID); 
-
-                // Construct the URL for the species information page
-                const infoPageURL = `https://im.chappuiscaetano.ch/php/species.php?id=${speciesID}`;
-                
-                // Redirect the user to the species information page
-                window.location.href = infoPageURL;
-            };
-
-            // Store image data to localStorage
-            localStorage.setItem('uploadedImage', e.target.result); 
+            centerImage.src = e.target.result;  // Update the src of the center image
+            localStorage.setItem('uploadedImage', e.target.result);  // Store image data to localStorage
             introText.style.display = "none"; 
-            previewHeading.style.display = "block";  
-            filePreview.style.display = "none";  
+            previewHeading.style.display = "block";  // Hide the preview heading
+            filePreview.style.display = "none";  // Hide the file preview
         };
         reader.readAsDataURL(fileInput.files[0]);
     }
