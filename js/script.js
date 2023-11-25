8524f5488619435f54d4aade32a0e0b147892e0b
cmchappuis <caetano.chappuis@stud.fhgr.ch>
2023-11-25 18:12:55 +0100

    works now

--- js/script.js
+++ js/script.js
@@ -1,4 +1,50 @@
 
+// Uplaod
+// Uplaod
+// Uplaod
+
+document.getElementById('upload-form').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent the default form submission
+
+    const formData = new FormData(this);
+    const imageInput = document.getElementById('image-upload');
+
+    // Debugging: Log the contents of formData
+    for (let [key, value] of formData.entries()) { 
+        console.log(`${key}: `, value);
+    }
+
+    if (imageInput.files[0]) {
+        // Send the file to the server using fetch API
+        fetch('/php/upload.php', {
+            method: 'POST',
+            body: formData,
+        })
+        
+        .then(response => {
+            // Check if the response is OK
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(data => {
+            try {
+                const jsonData = JSON.parse(data); // Parse the text as JSON
+                console.log('Success:', jsonData);
+            } catch (e) {
+                console.error('Error parsing JSON:', e);
+                console.error('Received data:', data);
+            }
+        })
+    } 
+});
+
+// machinelearning
+// machinelearning
+// machinelearning
+
+
 // Define the URL of the model
 const modelURL = 'https://teachablemachine.withgoogle.com/models/ORHSpBtYq/';
 
--- php/image_list.php
+++ php/image_list.php
@@ -2,6 +2,7 @@
 include 'config.php';  // Ensure this path is correct
 
 $directory = __DIR__ . '/uploads/';  // Use an absolute path
+
 $images = array_merge(
     glob($directory . "*.jpg"),
     glob($directory . "*.jpeg"),
